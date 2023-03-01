f9cbd7fa1c484b2ef084d6ec0a9280aa8c07d9e5
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-03-01 10:25:22 +0100

    POM

--- src/test/java/pages/QdPage.java
+++ src/test/java/pages/QdPage.java
@@ -7,7 +7,7 @@ import utilities.Driver;
 
 public class QdPage {
 
-    QdPage(){
+    public QdPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
--- src/test/java/tests/day11_POM_assertions/C02_PozitifLoginTesti.java
+++ src/test/java/tests/day11_POM_assertions/C02_PozitifLoginTesti.java
@@ -1,6 +1,9 @@
 package tests.day11_POM_assertions;
 
+import org.checkerframework.checker.units.qual.C;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.QdPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
@@ -13,6 +16,16 @@ public class C02_PozitifLoginTesti {
         Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
 
         //login linkine tiklayin
+        QdPage qdPage=new QdPage();
+        qdPage.ilkLogin.click();
+
+        //gecerli kullanici adi ve password ile giris yapin
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecerliUsername"));
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecerliPassword"));
+        qdPage.loginButonu.click();
+
+        //basarili sekilde giris yapildigini test edin
+
 
 
 
